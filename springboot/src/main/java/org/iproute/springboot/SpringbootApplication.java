074869ba0c3665521a964576af23ab105e836b2f
winterfell <mimotronik@gmail.com>
2021-09-06 10:14:40 +0800

    redis

---
+++ springboot/src/main/java/org/iproute/springboot/SpringbootApplication.java
@@ -0,0 +1,16 @@
+package org.iproute.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author winterfell
+ */
+@SpringBootApplication
+public class SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+}
